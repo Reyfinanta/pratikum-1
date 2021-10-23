ab837c451ab433e9ca12619d7bee7966c2e926b2
reyfinanta <reyfinanta2@gmail.com>
2021-10-23 22:39:27 +0700

    menambahkan file program

---
+++ file pemrograman/main.py
@@ -0,0 +1,26 @@
+print ("Assalamualaikum")
+print ("Let me introduce my self")
+
+data = input("My Name Is:")
+print("Nama Lengkap = ",data,)
+
+data = input("But you can call me:")
+print("Nama Panggilan = ",data,)
+
+data = input("My NPM Is:")
+print("npm=",data)
+
+data = input("I Was Born in:")
+print("Tempat lahir = ",data,)
+
+data_int = int(input("umur: "))
+print("umur = ",data_int,"years old",)
+
+data = input("iam very glad if you want to invite my house in:")
+print("Alamat =",data)
+
+data_int = int(input("so, dont forget to call me before with the number:"))
+print("no telepon:" ,data_int)
+
+data = input("my hobby is:")
+print("hobby saya = ",data,)    
